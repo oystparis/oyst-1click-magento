e8b0a386e72ca01d133d9033b888f56ae720b661
Mehdi Chaouch <mehdi@advocodo.com>
2017-07-20 16:03:50 +0200

    Improve the message when sync catalog #32

--- js/oyst/oneclick.js
+++ js/oyst/oneclick.js
@@ -5,7 +5,8 @@ function goToThis(url) {
 }
 
 window.onload = function onLoad() {
-    if (!jQuery("#syncbutton button").data("sync")) {
+    var syncBtn = $$('#syncbutton button')[0];
+    if (!syncBtn.readAttribute('data-sync')) {
         return;
     }
 
@@ -28,15 +29,52 @@ window.onload = function onLoad() {
     circle.set(0);
 
     function getImportProgress() {
-        jQuery.getJSON(jQuery("#syncbutton button").data("sync-url"), function (data) {
-            jQuery("#remaining").html((data['totalCount'] - data['remaining']) + '/');
-            jQuery("#totalCount").html(data['totalCount']);
-            circle.animate((data['totalCount'] - data['remaining'])/data['totalCount']);
-            if (0 < data['remaining']) {
-                setTimeout(getImportProgress, 1000);
+        var url = syncBtn.readAttribute('data-sync-url');
+        var request = new XMLHttpRequest();
+
+        request.open('GET', url, true);
+
+        request.onreadystatechange = function() {
+            if (this.readyState === 4) {
+                if (this.status >= 200 && this.status < 400) {
+                    // Success!
+                    var data = JSON.parse(this.responseText);
+                    var remainingEl = $('remaining');
+                    var totalCountEl = $('totalCount');
+
+                    remainingEl.update((data['totalCount'] - data['remaining']) + '/');
+                    totalCountEl.update(data['totalCount']);
+
+                    circle.animate((data['totalCount'] - data['remaining']) / data['totalCount']);
+                    if (0 < data['remaining']) {
+                        setTimeout(getImportProgress, 1000);
+                    }
+                    if (!data['remaining']) {
+                        syncBtn.disable();
+                        syncBtn.setStyle({background: ''});
+                        var msg = $('syncbutton').readAttribute('data-msg-success');
+                        showMessage(msg, "success");
+                    }
+                } else {
+                    // Error :(
+                    var msg = $('syncbutton').readAttribute('data-msg-error');
+                    showMessage(msg, "error");
+                }
             }
-        });
+        };
+
+        request.send();
+        request = null;
     }
 
     getImportProgress();
 };
+
+function showMessage(txt, type) {
+    var html = '<ul class="messages"><li class="' + type + '-msg"><ul><li>' + txt + '</li></ul></li></ul>';
+    $("messages").update(html);
+    var url = location.href;
+    var n = url.indexOf("#");
+    url = url.substring(0, n != -1 ? n : url.length);
+    location.replace(url + "#html-body");
+}
