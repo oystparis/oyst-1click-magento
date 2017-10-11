fb9d38f54687516ea862a2dcfebad1b98c35ca53
Mehdi Chaouch <mehdichaouch@users.noreply.github.com>
2017-10-11 18:23:55 +0200

    Feedback fix #101 (#107)
    * Remove 1-Click from menu and move 1-Click tab in sales
    
    * Change travis file
    
    * Move 1-Click inline JS in separate file
    
    * Use magento factory to define observer
    
    * JS validation for oyst url format

--- skin/frontend/base/default/js/oyst/oneclick.js
+++ skin/frontend/base/default/js/oyst/oneclick.js
@@ -11,7 +11,60 @@
 /**
  * OneClick Customs JS
  */
+"use strict";
 
+/**
+ * Main function to init 1-Click
+ *
+ * @param productId
+ * @param oneClickUrl
+ */
+function oystOneClick(productId, oneClickUrl) {
+    window.__OYST__ = window.__OYST__ || {};
+    window.__OYST__.getOneClickURL = function (cb, opts) {
+        opts = opts || {};
+
+        ready(function () {
+            var qty = $$("input[name='qty']")[0].value || 1;
+
+            var form = new FormData();
+            form.append("productRef", productId);
+            form.append("quantity", qty);
+            form.append("variationRef", getSimpleProductId());
+
+            var settings = {
+                async: true,
+                cacheControl: "no-cache",
+                data: form,
+                method: "POST",
+                mimeType: "multipart/form-data",
+                url: oneClickUrl
+            };
+
+            var xhr = new XMLHttpRequest();
+            xhr.open(settings.method, settings.url, settings.async);
+            xhr.setRequestHeader('cache-control', settings.cacheControl);
+            xhr.onload = function () {
+                if (200 === xhr.status) {
+                    var data = JSON.parse(xhr.responseText);
+                    var isErrorsInForm = null;
+                    // If not the preload of button run form validation to know if they are errors
+                    if (!opts.preload) {
+                        var isErrorsInForm = !isOystOneClickButtonFormValid('product_addtocart_form');
+                    }
+                    cb(isErrorsInForm, data.url);
+                }
+            };
+            xhr.send(form);
+        });
+    };
+}
+
+/**
+ * Wait for dom ready
+ *
+ * @param fn
+ */
 function ready(fn) {
     if (document.attachEvent ? 'complete' === document.readyState : 'loading' !== document.readyState) {
         fn();
