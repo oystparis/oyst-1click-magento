6be62fc5eeae1f237509f08f88a6838823d1b394
rus-daniel <daniel.rus@osf-global.com>
2018-04-16 17:57:50 +0300

    Disable product page button (#320) (#321)

--- app/code/community/Oyst/OneClick/Model/System/Config/Source/Catalog/Product/Button/Position.php
+++ app/code/community/Oyst/OneClick/Model/System/Config/Source/Catalog/Product/Button/Position.php
@@ -22,6 +22,7 @@ class Oyst_OneClick_Model_System_Config_Source_Catalog_Product_Button_Position
     public function toOptionArray()
     {
         return array(
+            array('value' => 'disabled','label' => Mage::helper('oyst_oneclick')->__('Disabled')),
             array('value' => 'before', 'label' => Mage::helper('oyst_oneclick')->__('Before add to cart button')),
             array('value' => 'after', 'label' => Mage::helper('oyst_oneclick')->__('After add to cart button')),
         );
