d6d26d9d46fea6c62dc9b357389d5e3bb251e101
Matthieu BOUCHOT <matthieu.bouchot@oyst.com>
2019-02-20 13:14:27 +0100

    Refactoring for set properly tracker parameters

--- app/code/community/Oyst/OneClick/Block/Tracking.php
+++ app/code/community/Oyst/OneClick/Block/Tracking.php
@@ -4,16 +4,33 @@ class Oyst_OneClick_Block_Tracking extends Mage_Core_Block_Template
 {
     public function getCheckoutOnepageSuccessTrackingParameters()
     {
-        $order = Mage::getModel('sales/order')->load(Mage::getSingleton('checkout/session')->getLastOrderId());
         return array(
+            'version' => 1,
+            'type' => 'track',
+            'event' => 'Confirmation Displayed',
+        );
+    }
+
+    public function getCheckoutOnepageSuccessTrackingExtraParameters()
+    {
+        $order = Mage::getModel('sales/order')->load(Mage::getSingleton('checkout/session')->getLastOrderId());
+        $extraParameters = array(
             'amount' => $order->getGrandTotal(),
             'paymentMethod' => $order->getPayment()->getMethod(),
             'currency' => $order->getOrderCurrencyCode(),
             'referrer' => urlencode(Mage::helper('core/url')->getCurrentUrl()),
             'merchantId' => Mage::getStoreConfig('oyst_oneclick/general/merchant_id'),
+            'orderId' => $order->getIncrementId(),
+            'userEmail' => $order->getCustomerEmail(),
         );
+
+        if ($order->getCustomerId()) {
+            $extraParameters['userId'] = $order->getCustomerId();
+        }
+
+        return $extraParameters;
     }
-    
+
     public function isEnabled()
     {
         return Mage::getStoreConfig('oyst_oneclick/general/enabled');
